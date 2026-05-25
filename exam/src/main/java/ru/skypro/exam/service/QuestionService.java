389fedaa354d3a6a4fc75df532afcc92b4ce6b0f
kat04-mal <k-04maleeva@yandex.ru>
2026-05-25 13:25:06 +0300

    Выполнение дз

---
+++ exam/src/main/java/ru/skypro/exam/service/QuestionService.java
@@ -0,0 +1,18 @@
+package ru.skypro.exam.service;
+
+import ru.skypro.exam.model.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+}
