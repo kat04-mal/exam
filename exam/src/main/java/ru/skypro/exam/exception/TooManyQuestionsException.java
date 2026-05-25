f73d96474876d72ad05131b2cf36da7aa72b71ec
kat04-mal <k-04maleeva@yandex.ru>
2026-05-25 13:31:51 +0300

    Выполнение курсовой работы

---
+++ exam/src/main/java/ru/skypro/exam/exception/TooManyQuestionsException.java
@@ -0,0 +1,12 @@
+package ru.skypro.exam.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class TooManyQuestionsException extends RuntimeException {
+
+    public TooManyQuestionsException(String message) {
+        super(message);
+    }
+}
